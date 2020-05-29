8c807b83d6a8d0e2311aec457e3bc0f4362f5e40
Maruchi Kim <maruchi.kim@gmail.com>
2020-05-29 12:28:41 -0700

    Initial Commit

---
+++ alpha-comms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alpha-comms
+//
+//  Created by Maruchi Kim on 5/29/20.
+//  Copyright © 2020 Maruchi Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
